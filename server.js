d682a481997d6fc46ad9d6a6fb10a5db023bcef6
Settakit Thasri <settakit@sirisoft.co.th>
2021-02-24 15:45:31 +0700

    Try to git and build auto 7

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.get('/testpath6', (req, res) => {
   res.send('hello test path 6').status(200)
 })
 
+app.get('/testpath7', (req, res) => {
+  res.send('hello test path 7').status(200)
+})
+
 // app.get('/*', (req, res) => {
 //     res.redirect('/')
 // })
