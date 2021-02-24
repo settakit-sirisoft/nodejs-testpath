e9b5b74c3e0ad32327e75021893bc72775bbbf13
Settakit Thasri <settakit@sirisoft.co.th>
2021-02-24 15:27:24 +0700

    Try to git and build auto 6

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.get('/testpath4', (req, res) => {
   res.send('hello test path 4').status(200)
 })
 
-// app.get('/testpath5', (req, res) => {
-//   res.send('hello test path 5').status(200)
-// })
+app.get('/testpath5', (req, res) => {
+  res.send('hello test path 5').status(200)
+})
 
 app.get('/testpath6', (req, res) => {
   res.send('hello test path 6').status(200)
